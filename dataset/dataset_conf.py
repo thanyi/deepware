dd48f8b658d2f9afef1e56f80ed3499c808686a4
ethaiyi9 <yiceyuan@126.com>
2022-04-01 11:58:02 +0800

    change dataset.py

--- dataset/dataset_conf.py
+++ dataset/dataset_conf.py
@@ -1,14 +1,12 @@
 ###########################
 #  路径
 ###########################
-normal_root = r"/content/data/normal_dlib"
-malicious_root = r"/content/data/Deepfakes_dlib"
+normal_root = r"/content/data/ff_original"
+malicious_root = r"/content/data/faceswap"
 csv_root = r"/content/data/csv"
 
 
-malicious_root_deepfake = r"/content/data/Deepfakes_dlib"
-malicious_root_deepfake_compress = r"/content/data/DeepFake++compress_dlib"
-malicious_root_faceswap = r"/content/data/FaceSwap_dlib"
+
 
 
 model_name = "weights/deepware.pt"
